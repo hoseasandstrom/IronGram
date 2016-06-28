5fb614093e58bba4af28356ca37a5d64743735af
hoseasandstrom <Lakyn4ever>
2016-06-28 11:33:28 -0400

    Create entities  & services

---
+++ src/main/java/com/theironyard/controllers/IronGramRestController.java
@@ -0,0 +1,18 @@
+package com.theironyard.controllers;
+
+import com.theironyard.services.PhotoRepository;
+import com.theironyard.services.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by hoseasandstrom on 6/28/16.
+ */
+@RestController
+public class IronGramRestController {
+    @Autowired
+    UserRepository users;
+
+    @Autowired
+    PhotoRepository photos;
+}
